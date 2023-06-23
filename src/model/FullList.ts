8d9cbba00c46837446293c0c2a717646c4ef9a5f
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-06-23 11:05:17 -0300

    My solution not finished and not working

---
+++ src/model/FullList.ts
@@ -0,0 +1,19 @@
+import ListItem from "./listItem";
+
+interface List {
+    list: ListItem[],
+    load(): void,
+    save(): void,
+    clearList(): void,
+    addItem(itemObj: ListItem): void,
+    removeItem(id: string): void,
+}
+
+export default class FullList implements List {
+
+    load() {
+
+    }
+
+
+}
