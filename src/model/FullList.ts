8e5ede4ed6e247805a44b593f9a791ffd1dfe7bd
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-06-23 12:37:29 -0300

    Implementing the FullList Class - Mytry
    All but the load method were created
    1. Tutorial att 19:18
    2. Implement Load for challenge Fulllist
    3. Go to template challenge

--- src/model/FullList.ts
+++ src/model/FullList.ts
@@ -11,9 +11,33 @@ interface List {
 
 export default class FullList implements List {
 
-    load() {
+    static instance: FullList = new FullList()
+    private constructor(private _list: FullList[] = []) { }
+
+    get list(): ListItem[] {
+        return this.list
+    }
+
+    load(): void {
 
     }
 
+    save(): void {
+        localStorage.setItem("myList", JSON.stringify(this._list))
+    }
+
+    clearlist(): void {
+        this._list = []
+        this.save()
+    }
+
+    addItem(): void {
+
+    }
+
+    removeItem(id: string): void {
+        this._list = this._list.filter(item => item.id !== id)
+        this.save()
+    }
 
 }
