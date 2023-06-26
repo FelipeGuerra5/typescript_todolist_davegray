0a65eb1495cf5f185dc003b32caecfb44b7aa578
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-06-26 11:51:24 -0300

    Requirements for the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 import './css/style.css'
+
+// Import all we created
+
+// init app
+
+  // Take the listener to the form entry event
+  // It will handle the functionality to:
+  // create a new item
+  // clear items.
+
+
+
+
+
+// javascript only loads after the document is loaded addEventListener to docuemnt
